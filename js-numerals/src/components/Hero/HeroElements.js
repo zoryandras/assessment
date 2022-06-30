e9072f1a65ed6d43b1446c88bbdf10a664bd0fa9
Andras Varga <zoryandras@gmail.com>
2022-06-30 19:16:27 +0200

    Changed HeroContainer size

--- js-numerals/src/components/Hero/HeroElements.js
+++ js-numerals/src/components/Hero/HeroElements.js
@@ -6,7 +6,7 @@ export const HeroContainer = styled.div`
   justify-content: center;
   align-items: center;
   padding: 0 30px;
-  height: 900px;
+  height: 920px;
   position: relative;
 
   @media screen and (max-width: 768px) {
