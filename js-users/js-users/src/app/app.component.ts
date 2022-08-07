0142024ca53598057def575e78fa36e4b022d5ec
Andras Varga <zoryandras@gmail.com>
2022-08-07 22:49:44 +0200

    add: opendialog(), getAllUser() created for the data to be shown.

--- js-users/js-users/src/app/app.component.ts
+++ js-users/js-users/src/app/app.component.ts
@@ -1,10 +1,100 @@
-import { Component } from '@angular/core';
+import { ApiService } from './services/api.service';
+import { DialogComponent } from './dialog/dialog.component';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AfterViewInit, Component, ViewChild, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { style } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'js-users';
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = [
+    'first_name',
+    'last_name',
+    'created_at',
+    'action',
+  ];
+  dataSource!: MatTableDataSource<any>;
+  selectedRow: any[] = [];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(public dialog: MatDialog, private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.getAllUser();
+  }
+
+  openDialog() {
+    this.dialog
+      .open(DialogComponent, {
+        width: '30%',
+      })
+      .afterClosed()
+      .subscribe((val) => {
+        if (val === 'save') {
+          this.getAllUser();
+        }
+      });
+  }
+
+  getAllUser() {
+    this.api.getUser().subscribe({
+      next: (res) => {
+        // console.log(res);
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error: (err) => {
+        alert(err);
+      },
+    });
+  }
+
+  editUser(row: any) {
+    this.dialog
+      .open(DialogComponent, {
+        width: '30%',
+        data: row,
+      })
+      .afterClosed()
+      .subscribe((val) => {
+        if (val === 'update') {
+          this.getAllUser();
+        }
+      });
+  }
+  deleteUser(id: number) {
+    this.api.deleteUser(id).subscribe({
+      next: (res) => {
+        alert('User deleted successfully');
+        this.getAllUser();
+      },
+      error: (err) => {
+        alert(err);
+      },
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  lockUser(row: any) {
+    this.selectedRow.find((x) => x == row)
+      ? (this.selectedRow = this.selectedRow.filter((x) => x != row))
+      : this.selectedRow.push(row);
+  }
 }
