2b0c18b9b74a7591507b5ee545c1e207737dbd16
Andras Varga <zoryandras@gmail.com>
2022-08-07 22:27:04 +0200

    build: added api services

--- js-users/js-users/src/app/services/api.service.ts
+++ js-users/js-users/src/app/services/api.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  postUser(data: any) {
+    return this.http.post<any>(`http://localhost:3000/users`, data);
+  }
+
+  getUser() {
+    return this.http.get<any>(`http://localhost:3000/users`);
+  }
+
+  putUser(data: any, id: number) {
+    return this.http.put<any>(`http://localhost:3000/users/${id}`, data);
+  }
+
+  deleteUser(id: number) {
+    return this.http.delete<any>(`http://localhost:3000/users/${id}`);
+  }
 }
