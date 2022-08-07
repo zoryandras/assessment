f9d1c4edc0f908d02e676a278a73178ef3cf112c
Andras Varga <zoryandras@gmail.com>
2022-08-07 17:38:42 +0200

    build: added getUsers() to services.

--- js-users/js-users/src/app/user/user.service.ts
+++ js-users/js-users/src/app/user/user.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private apiServerUrl = environment.apiBaseUrl
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  public getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiServerUrl}`);
+  }
 }
